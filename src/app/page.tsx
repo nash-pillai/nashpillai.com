4dda3f475bee8622162f0488aaf85ecb8d0bea3d
Nash Pillai <nash.pillai@protonmail.com>
2024-07-28 21:11:35 -0400

    setup shadcn and magic ui and add fancy text reveals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,44 @@
+/* eslint-disable react/jsx-key */
+import GradualSpacing from "@/components/magicui/gradual-spacing";
+import WordPullUp from "@/components/magicui/word-pull-up";
 import Link from "next/link";
 
 export default function HomePage() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-				<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-					Hi! I&apos;m <span className="text-[hsl(280,100%,70%)]">Nash</span>!
-				</h1>
+				<WordPullUp
+					words={["Hi!", " ", "I'm", <span className="text-[hsl(280,100%,70%)]">Nash</span>, "!"]}
+					className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]"
+				/>
 				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
 					<Link
 						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
 						href="https://www.linkedin.com/in/nash-pillai/"
 						target="_blank">
 						<h3 className="text-2xl font-bold">My Linkedin →</h3>
-						<div className="text-lg">Keep up with my latest projects.</div>
+						<GradualSpacing className="text-lg" text="Keep up with my latest projects." />
 					</Link>
 					<Link
 						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
 						href="https://github.com/nash-pillai/"
 						target="_blank">
 						<h3 className="text-2xl font-bold">My Github →</h3>
-						<div className="text-lg">See all my other cool websites.</div>
+						<GradualSpacing className="text-lg" text="See all my other cool websites." />
 					</Link>
 					<Link
 						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
 						href="mailto:nash.pillai@protonmail.com"
 						target="_blank">
 						<h3 className="text-2xl font-bold">Contact Me →</h3>
-						<div className="text-lg">Send me an email.</div>
+						<GradualSpacing className="text-lg" text="Send me an email." />
 					</Link>
 					<Link
 						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
 						href="/resume"
 						target="_blank">
 						<h3 className="text-2xl font-bold">My Resume →</h3>
-						<div className="text-lg">A quick sample of my work.</div>
+						<GradualSpacing className="text-lg" text="A quick sample of my work." />
 					</Link>
 				</div>
 			</div>
---
+++ src/components/magicui/gradual-spacing.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { AnimatePresence, motion, type Variants } from "framer-motion";
+
+import { cn } from "@/lib/utils";
+
+interface GradualSpacingProps {
+	text: string;
+	duration?: number;
+	delayMultiple?: number;
+	framerProps?: Variants;
+	className?: string;
+}
+
+export default function GradualSpacing({
+	text,
+	duration = 0.1,
+	delayMultiple = 0.04,
+	framerProps = {
+		hidden: { opacity: 0, x: -20 },
+		visible: { opacity: 1, x: 0 },
+	},
+	className,
+}: GradualSpacingProps) {
+	return (
+		<div className="flex">
+			<AnimatePresence>
+				{text.split("").map((char, i) => (
+					<motion.div
+						key={i}
+						initial="hidden"
+						animate="visible"
+						exit="hidden"
+						variants={framerProps}
+						transition={{ duration, delay: i * delayMultiple }}
+						className={cn("drop-shadow-sm", className)}>
+						{char === " " ? <span>&nbsp;</span> : char}
+					</motion.div>
+				))}
+			</AnimatePresence>
+		</div>
+	);
+}
