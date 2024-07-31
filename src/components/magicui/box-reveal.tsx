ae6032094763a37c1fee813f63fd60aed7a92880
Nash Pillai <nash.pillai@protonmail.com>
2024-07-31 18:09:16 -0400

    format box reveal

--- src/components/magicui/box-reveal.tsx
+++ src/components/magicui/box-reveal.tsx
@@ -4,68 +4,63 @@ import { useEffect, useRef } from "react";
 import { motion, useAnimation, useInView } from "framer-motion";
 
 interface BoxRevealProps {
-  children: JSX.Element;
-  width?: "fit-content" | "100%";
-  boxColor?: string;
-  duration?: number;
+	children: JSX.Element;
+	width?: "fit-content" | "100%";
+	boxColor?: string;
+	duration?: number;
 }
 
-export const BoxReveal = ({
-  children,
-  width = "fit-content",
-  boxColor,
-  duration,
-}: BoxRevealProps) => {
-  const mainControls = useAnimation();
-  const slideControls = useAnimation();
+export const BoxReveal = ({ children, width = "fit-content", boxColor, duration }: BoxRevealProps) => {
+	const mainControls = useAnimation();
+	const slideControls = useAnimation();
 
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true });
 
-  useEffect(() => {
-    if (isInView) {
-      slideControls.start("visible");
-      mainControls.start("visible");
-    } else {
-      slideControls.start("hidden");
-      mainControls.start("hidden");
-    }
-  }, [isInView, mainControls, slideControls]);
+	useEffect(() => {
+		if (isInView) {
+			void slideControls.start("visible");
+			void mainControls.start("visible");
+		} else {
+			void slideControls.start("hidden");
+			void mainControls.start("hidden");
+		}
+	}, [isInView, mainControls, slideControls]);
 
-  return (
-    <div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
-      <motion.div
-        variants={{
-          hidden: { opacity: 0, y: 75 },
-          visible: { opacity: 1, y: 0 },
-        }}
-        initial="hidden"
-        animate={mainControls}
-        transition={{ duration: duration ? duration : 0.5, delay: 0.25 }}
-      >
-        {children}
-      </motion.div>
+	return (
+		<div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
+			<motion.div
+				variants={{
+					hidden: { opacity: 0, y: 75 },
+					visible: { opacity: 1, y: 0 },
+				}}
+				initial="hidden"
+				animate={mainControls}
+				transition={{ duration: duration ? duration : 0.5, delay: 0.25 }}
+			>
+				{children}
+			</motion.div>
 
-      <motion.div
-        variants={{
-          hidden: { left: 0 },
-          visible: { left: "100%" },
-        }}
-        initial="hidden"
-        animate={slideControls}
-        transition={{ duration: duration ? duration : 0.5, ease: "easeIn" }}
-        style={{
-          position: "absolute",
-          top: 4,
-          bottom: 4,
-          left: 0,
-          right: 0,
-          zIndex: 20,
-          background: boxColor ? boxColor : "#5046e6",
-        }}
-      />
-    </div>
-  );
+			<motion.div
+				variants={{
+					hidden: { left: 0 },
+					visible: { left: "100%" },
+				}}
+				initial="hidden"
+				animate={slideControls}
+				transition={{ duration: duration ? duration : 0.5, ease: "easeIn" }}
+				style={{
+					position: "absolute",
+					top: 4,
+					bottom: 4,
+					left: 0,
+					right: 0,
+					zIndex: 20,
+					background: boxColor ? boxColor : "#5046e6",
+				}}
+			/>
+		</div>
+	);
 };
 
 export default BoxReveal;
