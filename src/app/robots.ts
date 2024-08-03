27963ef21de9614d21b09870f53481a5316673f4
Nash Pillai <nash.pillai@protonmail.com>
2024-08-02 22:30:20 -0400

    add robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { env } from "@/env";
+import { type MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+	return {
+		rules: {
+			userAgent: "*",
+			allow: "/",
+		},
+		sitemap: `${env.URL}/sitemap.xml`,
+	};
+}
